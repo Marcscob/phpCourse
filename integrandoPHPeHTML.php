c2498cbb13c5bb8447101242a6d51fade16365d9
Marcscob <marcscob@hotmail.com>
2023-11-21 08:05:03 -0300

    pk

--- integrandoPHPeHTML.php
+++ integrandoPHPeHTML.php
@@ -1,13 +1,18 @@
 
-<?php
-  include './meuIndex.php';
-?>
+
 
 <h1>
 <?php
   echo "teste1 <br>";
   echo "teste2";
+ 
 ?>
 </h1>
 
 <div><span><h2>estamos no ano de <?= date('Y'); ?></h2></span></div>
+
+<?php
+  include './meuIndex.php';
+?>
+
+<?= 'este é um teste' ?>
